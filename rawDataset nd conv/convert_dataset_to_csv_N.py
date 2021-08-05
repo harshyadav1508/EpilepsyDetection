cd62905210a91e9497ffa26aeea095e19dc3d03e
harshyadav1508 <55807854+harshyadav1508@users.noreply.github.com>
2021-08-05 12:08:21 +0530

    raw dataset and conv of dataset to csv py file

---
+++ rawDataset nd conv/convert_dataset_to_csv_N.py
@@ -0,0 +1,21 @@
+filecnt = 100
+folderName = "D:\iit_thesis\dataset_eeg\\N\\N"
+
+data = ["" for i in range(filecnt)]
+  
+# Reading data from file1 
+
+for i in range(1,filecnt+1):
+    name = folderName + str(i).zfill(3) + ".txt"
+    with open(name) as fp:
+        data[i-1] = list(fp.read().split('\n'))
+
+fin = zip(*data)
+# print(data)
+t = [list(a) for a in fin]
+# print(t)
+import csv
+with open('C.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    for data in t:
+        writer.writerow(data)
